ed8a3fe9b26aae17e270d6bd43836f01beb05a37
BT_1N3_25 <MeiSamuel2001@gmail.com>
2019-05-21 14:43:13 -0400

    cool af dude

--- src/runner.java
+++ src/runner.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class runner
 {
     public static void main(String[] bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb)
@@ -16,5 +18,13 @@ public class runner
         tr.addDailySteps(1111);
         System.out.println(tr.activeDays());
         System.out.println(tr.averageSteps());
+        System.out.println("\n");
+        q3 delim = new q3("{","}");
+        String[] a = {"{","b","a","s","d","}"};
+        ArrayList<String> b = delim.getDelimitersList(a);
+        System.out.println(delim.toString(b));
+        System.out.println(delim.isBalanced(b));
+
+
     }
 }
